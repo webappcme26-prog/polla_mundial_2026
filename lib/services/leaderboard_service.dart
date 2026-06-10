bf00981ee0b367a109f09ca6c90791a12963d861
webappcme26-prog <webappcme26@gmail.com>
2026-06-10 10:41:17 -0500

    Corregir leaderboard y excluir admin del ranking

--- lib/services/leaderboard_service.dart
+++ lib/services/leaderboard_service.dart
@@ -3,21 +3,22 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 class LeaderboardService {
   final SupabaseClient _supabase = Supabase.instance.client;
 
-  Future<List<Map<String, dynamic>>> getLeaderboard() async {
-    final response = await _supabase
-        .from('leaderboard')
-        .select('''
-          user_id,
-          nombre,
-          puntos_totales,
-          exactos,
-          aciertos_resultado
-        ''')
-        .order('puntos_totales', ascending: false)
-        .order('exactos', ascending: false)
-        .order('aciertos_resultado', ascending: false)
-        .order('nombre', ascending: true);
+ Future<List<Map<String, dynamic>>> getLeaderboard() async {
+  final response = await _supabase
+      .from('leaderboard')
+      .select('''
+        user_id,
+        nombre,
+        puntos_totales,
+        exactos,
+        aciertos_resultado
+      ''')
+      .neq('user_id', 'e5f35cb4-7a3e-48b4-987e-1eaafa2544b8')
+      .order('puntos_totales', ascending: false)
+      .order('exactos', ascending: false)
+      .order('aciertos_resultado', ascending: false)
+      .order('nombre', ascending: true);
 
-    return List<Map<String, dynamic>>.from(response);
-  }
+  return List<Map<String, dynamic>>.from(response);
+}
 }
